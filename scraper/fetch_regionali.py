a797ae2eb986bf496ecb8c21625b4f490b8785c4
Massimo Grifoni <massimogrifoni58@gmail.com>
2026-07-20 09:54:43 +0200

    Fix indentation fetch_regionali

--- scraper/fetch_regionali.py
+++ scraper/fetch_regionali.py
@@ -163,9 +163,9 @@ def normalize_news(xml_bytes, limit):
     if xml_bytes is None:
         return []
     try:
-    root = ET.fromstring(xml_bytes)
-except ET.ParseError:
-    return []
+        root = ET.fromstring(xml_bytes)
+    except ET.ParseError:
+        return []
     items = root.findall("./channel/item")[:limit]
 
     posts = []
